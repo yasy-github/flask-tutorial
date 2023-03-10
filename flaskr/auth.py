e152b6ab73d935bde92ea24172f6f70875c3aae9
yasy-github <whangyasy@gmail.com>
2023-03-10 11:01:32 +0700

    [FIXED] - Call fetchone() instead of assignning

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -72,7 +72,7 @@ def load_logged_in_user():
     else:
         g.user = get_db().execute(
             "SELECT * FROM user WHERE id = ?", (user_id,)
-        ).fetchone
+        ).fetchone()
 
 @bp.route('/logout')
 def logout():
